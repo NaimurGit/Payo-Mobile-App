87d5826a1fd0c94f14477a41c6d663bac89382e4
NaimurGit <naimur.turjo03@gmail.com>
2025-03-12 20:39:25 +0600

    first commit

---
+++ script/login.js
@@ -0,0 +1,18 @@
+document.getElementById("login-btn").addEventListener("click",
+    function (event) {
+      event.preventDefault();
+      const accountNumber = document.getElementById("account-number").value;
+      const Pin = document.getElementById("pin").value;
+
+      if (accountNumber.length === 11){
+        if (Pin==="1234"){
+            window.location.href="./main.html"
+         }
+         else{
+             alert("Please Give valid Pin")
+         } 
+      }
+      else{
+            alert("Your Account number is not Available")
+      }
+    })
